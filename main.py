97a1fb1a0846a4389e63d89b66333556c315c395
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2024-12-20 14:17:06 +0900

    don't put your access tokens and api keys in the code

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ import tweepy
 import sys
 
 my_keys = open('twitterkeys.txt', 'r').read().splitlines()
+
+api_key = my_keys[0]
+api_key_secret = my_keys[1]
+access_token = my_keys[2]
+access_token_secret = my_keys[3]
+
