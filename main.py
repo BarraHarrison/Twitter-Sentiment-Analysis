42c5ddd00cf7d95676781286267a8e82c5ac2cd6
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2024-12-20 14:20:35 +0900

    building the connection to the application

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ api_key_secret = my_keys[1]
 access_token = my_keys[2]
 access_token_secret = my_keys[3]
 
+# Building connection to the Twitter(X) app
+auth_handler = tweepy.OAuthHandler(consumer_key=api_key, consumer_secret=api_key_secret)
+auth_handler.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth_handler)
