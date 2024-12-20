382b6b2636c7ed7f1bc70df811faf7d1debe8264
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2024-12-20 14:28:39 +0900

    cleaning up the code

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
-# Python Twitter Sentiment Analysis
-
 from textblob import TextBlob
 import tweepy
 import sys
 
-my_keys = open('twitterkeys.txt', 'r').read().splitlines()
+# Read API keys from the file
+my_keys = {}
+with open('twitterkeys.txt', 'r') as file:
+    for line in file:
+        key, value = line.strip().split(" = ")
+        my_keys[key] = value
 
-api_key = my_keys[0]
-api_key_secret = my_keys[1]
-access_token = my_keys[2]
-access_token_secret = my_keys[3]
+# Assign keys to variables
+api_key = my_keys["api_key"]
+api_key_secret = my_keys["api_key_secret"]
+access_token = my_keys["access_token"]
+access_token_secret = my_keys["access_token_secret"]
 
-# Building connection to the Twitter(X) app
-auth_handler = tweepy.OAuthHandler(consumer_key=api_key, consumer_secret=api_key_secret)
+# Authenticate with Twitter
+auth_handler = tweepy.OAuthHandler(api_key, api_key_secret)
 auth_handler.set_access_token(access_token, access_token_secret)
 
+# Connect to the API
 api = tweepy.API(auth_handler)
+
+# Verify credentials
+try:
+    api.verify_credentials()
+    print("Authentication successful")
+except tweepy.TweepyException as e:
+    print("Error during authentication:", e)
+    sys.exit(1)
+
+# Search for tweets
+search_term = "stocks"
+tweet_amount = 20
+
+try:
+    tweets = tweepy.Cursor(api.search_tweets, q=search_term, lang="en").items(tweet_amount)
+    for tweet in tweets:
+        print(tweet.text)
+except tweepy.TweepyException as e:
+    print("Error fetching tweets:", e)
